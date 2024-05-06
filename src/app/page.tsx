457e8ccc7abee5110965ae18d5b2f1e0012ed4db
Mantelioo <mantas992@gmail.com>
2024-05-06 23:22:08 +0300

    revert: renamed test to docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h1>TEST</h1>
+          <h2>Docs <span>-&gt;</span></h2>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
 
