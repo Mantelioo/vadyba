5b6d65417324aec674f25aa351ba03cd0ded1633
astrodomas <astradominykas@gmail.com>
2024-05-19 21:09:39 +0300

    add: reference MY_SECRET

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
-        <h1>{Date.now()}</h1>
+        <h1>{process.env.MY_SECRET}</h1>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
